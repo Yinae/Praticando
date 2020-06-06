7dc729924def7953d59963b9560ee11f9a57683a
Yinaê <yinae04@gmail.com>
2020-06-06 14:17:39 -0300

    Adicionando arquivos

---
+++ parteB.js
@@ -0,0 +1,6 @@
+for (var i = 0; i <= photos.length; i++) {
+
+    var photo = document.createElement('img');
+    photo.src = photos[i];
+    exemploDoAcacio.append(photo);
+}
---
+++ test.js
@@ -0,0 +1,42 @@
+alert('Potterhead');
+
+var exemploDoAcacio = document.getElementById('content');
+console.log(exemploDoAcacio)
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://www.okchicas.com/wp-content/uploads/2019/05/Fondos-de-pantalla-inspirados-Harry-Potter-2-394x700.jpg';
+
+exemploDoAcacio.append(novaFoto);
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://www.okchicas.com/wp-content/uploads/2019/05/Fondos-de-pantalla-inspirados-Harry-Potter-3-394x700.jpg';
+exemploDoAcacio.append(novaFoto);
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://www.okchicas.com/wp-content/uploads/2019/05/Fondos-de-pantalla-inspirados-Harry-Potter-12-394x700.jpg';
+exemploDoAcacio.append(novaFoto);
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://www.okchicas.com/wp-content/uploads/2019/05/Fondos-de-pantalla-inspirados-Harry-Potter-13-394x700.jpg';
+exemploDoAcacio.append(novaFoto);
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://i.pinimg.com/474x/ce/9a/9b/ce9a9b91574e4c79eaa4f70c126c3b8b--cel-string-art.jpg?b=t';
+exemploDoAcacio.append(novaFoto);
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://i.pinimg.com/564x/46/1e/8e/461e8edebfb1f9df0cb394cfd2b5628d.jpg';
+exemploDoAcacio.append(novaFoto);
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://i.pinimg.com/originals/59/29/82/592982ea881f7c113ac2688417e7fe5c.jpg';
+exemploDoAcacio.append(novaFoto);
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://i.pinimg.com/originals/0c/be/05/0cbe05ca51f292dac1b40972c5d30895.jpg';
+exemploDoAcacio.append(novaFoto);
+
+var novaFoto = document.createElement('img')
+novaFoto.src = 'https://i.pinimg.com/originals/fb/ef/15/fbef15268f4e6e3db74402b5d3d4ded0.jpg';
+exemploDoAcacio.append(novaFoto);
+
